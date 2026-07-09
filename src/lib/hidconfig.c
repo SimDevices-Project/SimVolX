b0d7e8b3f436f5b8b00e22773571ee1bc1a282b3
Handle <mail@bysb.net>
2026-07-10 01:25:00 +0800

    refactor: remove hidconfig command implementations (ROLLER_GET_DATA, INPUT_MODE, LED_SET_MODE, SP_LED_SET, SP_INPUT)

--- src/lib/hidconfig.c
+++ src/lib/hidconfig.c
@@ -17,23 +17,6 @@ static HidconfigData *dataUpload  = (HidconfigData *)HIDCFG_Buffer_IN;
 uint8_t HIDCONFIG_Upload();
 void HIDCONFIG_Receive_Handler();
 
-volatile uint8_t sp_input_state = 0;
-
-void SP_INPUT_Upload()
-{
-  memset(HIDCFG_Buffer_IN, 0, 64);
-
-  dataUpload->reportID = HIDCONFIG_REPORT_ID;
-  dataUpload->symbol   = 0x00;
-  dataUpload->command  = SP_INPUT_GET;
-  dataUpload->state    = STATE_OK;
-
-  dataUpload->roller_value_sp = Roller_GetValue();
-  dataUpload->input_status    = KeyScan_GetAllKeyDebouncedStatus();
-
-  HIDCONFIG_Upload();
-}
-
 void HIDCONFIG_Get_SerialNumber(uint8_t *buf)
 {
   uint32_t Device_Serial0, Device_Serial1, Device_Serial2;
@@ -57,7 +40,6 @@ void HIDCONFIG_Get_SerialNumber(uint8_t *buf)
 
 void SP_INPUT_OnDataUpdate_Handler()
 {
-  SP_INPUT_Upload();
 }
 
 void HIDCONFIG_Receive_Handler()
@@ -84,92 +66,10 @@ void HIDCONFIG_Receive_Handler()
           HIDCONFIG_Upload();
           break;
         }
-        case ROLLER_GET_DATA: {
-          uint16_t rollerValue    = Roller_GetValue();
-          uint16_t rollerRawValue = Roller_GetRawValue();
-          dataUpload->command          = ROLLER_GET_DATA;
-          dataUpload->state            = STATE_OK;
-          dataUpload->roller_value     = rollerValue;
-          dataUpload->roller_raw_value = rollerRawValue;
-          HIDCONFIG_Upload();
-          break;
-        }
         case ROLLER_SET_OFFSET: {
           Roller_ResetOffset();
 
           dataUpload->command = ROLLER_SET_OFFSET;
-          dataUpload->state   = STATE_OK;
-          HIDCONFIG_Upload();
-          break;
-        }
-        case INPUT_MODE_GET: {
-          dataUpload->command     = INPUT_MODE_GET;
-          dataUpload->state       = STATE_OK;
-          dataUpload->device_mode = GlobalData->DeviceMode;
-          HIDCONFIG_Upload();
-          break;
-        }
-        case INPUT_MODE_SET: {
-          GlobalData->DeviceMode = dataReceive->device_mode;
-          dataUpload->command    = INPUT_MODE_SET;
-          dataUpload->state      = STATE_OK;
-          HIDCONFIG_Upload();
-          break;
-        }
-        case LED_SET_MODE: {
-          if (dataReceive->led_tag == LEDTAG_ALL) {
-            LED_RGB_SetAll(dataReceive->r, dataReceive->g, dataReceive->b);
-          } else {
-            if (dataReceive->led_index == 0xff) {
-              LED_RGB_SetPort(dataReceive->led_tag, dataReceive->r, dataReceive->g, dataReceive->b);
-            } else {
-              LED_RGB_Set(dataReceive->led_tag, dataReceive->led_index, dataReceive->r, dataReceive->g, dataReceive->b);
-            }
-          }
-
-          dataUpload->command = LED_SET_MODE;
-          dataUpload->state   = STATE_OK;
-          HIDCONFIG_Upload();
-          break;
-        }
-        case SP_LED_SET: {
-          dataUpload->state   = STATE_OK;
-          dataUpload->command = SP_LED_SET;
-          switch (dataReceive->board_id) {
-            case 0x00:
-              LED_RGB_Set(LED_LEFT, 0, dataReceive->led_rgb_left[0][0], dataReceive->led_rgb_left[0][1], dataReceive->led_rgb_left[0][2]);
-              LED_RGB_Set(LED_LEFT, 1, dataReceive->led_rgb_left[1][0], dataReceive->led_rgb_left[1][1], dataReceive->led_rgb_left[1][2]);
-              LED_RGB_Set(LED_LEFT, 2, dataReceive->led_rgb_left[2][0], dataReceive->led_rgb_left[2][1], dataReceive->led_rgb_left[2][2]);
-              LED_RGB_Set(LED_LEFT, 3, dataReceive->led_rgb_left[3][0], dataReceive->led_rgb_left[3][1], dataReceive->led_rgb_left[3][2]);
-              LED_RGB_Set(LED_LEFT, 4, dataReceive->led_rgb_left[4][0], dataReceive->led_rgb_left[4][1], dataReceive->led_rgb_left[4][2]);
-              LED_RGB_Set(LED_LEFT, 5, dataReceive->led_rgb_left[5][0], dataReceive->led_rgb_left[5][1], dataReceive->led_rgb_left[5][2]);
-              LED_RGB_Set(LED_RIGHT, 0, dataReceive->led_rgb_right[0][0], dataReceive->led_rgb_right[0][1], dataReceive->led_rgb_right[0][2]);
-              LED_RGB_Set(LED_RIGHT, 1, dataReceive->led_rgb_right[1][0], dataReceive->led_rgb_right[1][1], dataReceive->led_rgb_right[1][2]);
-              LED_RGB_Set(LED_RIGHT, 2, dataReceive->led_rgb_right[2][0], dataReceive->led_rgb_right[2][1], dataReceive->led_rgb_right[2][2]);
-              LED_RGB_Set(LED_RIGHT, 3, dataReceive->led_rgb_right[3][0], dataReceive->led_rgb_right[3][1], dataReceive->led_rgb_right[3][2]);
-              LED_RGB_Set(LED_RIGHT, 4, dataReceive->led_rgb_right[4][0], dataReceive->led_rgb_right[4][1], dataReceive->led_rgb_right[4][2]);
-              LED_RGB_Set(LED_RIGHT, 5, dataReceive->led_rgb_right[5][0], dataReceive->led_rgb_right[5][1], dataReceive->led_rgb_right[5][2]);
-              break;
-            default:
-              dataUpload->state = STATE_ERROR;
-              break;
-          }
-          break;
-        }
-        case SP_INPUT_GET: {
-          SP_INPUT_Upload();
-          break;
-        }
-        case SP_INPUT_GET_START: {
-          sp_input_state      = 1;
-          dataUpload->command = SP_INPUT_GET_START;
-          dataUpload->state   = STATE_OK;
-          HIDCONFIG_Upload();
-          break;
-        }
-        case SP_INPUT_GET_END: {
-          sp_input_state      = 0;
-          dataUpload->command = SP_INPUT_GET_END;
           dataUpload->state   = STATE_OK;
           HIDCONFIG_Upload();
           break;
